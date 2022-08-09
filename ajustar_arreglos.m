f7e1ef02f2c2b03c13e9520133a0b2a6c091191a
omarvlo <69394469+omarvlo@users.noreply.github.com>
2022-08-09 10:44:27 -0500

    Delete ajustar_arreglos.m

--- ajustar_arreglos.m
+++
@@ -1,8 +0,0 @@
-function [reconocidos_out originales_out] = ajustar_arreglos(reconocidos_in,originales_in)
-
-[filas_rec columnas_rec] = size(reconocidos_in);
-[filas_orig columnas_orig] = size(originales_in);
-
-reconocidos_out = reconocidos_in(1:filas_rec,:);
-
-originales_out = originales_in(1:filas_rec,:);
